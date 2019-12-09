a8ee56e2fc8697a94b5d15cdc510b6ac5641b7be
Madeline Hightower <mhighto1@nd.edu>
2019-12-09 10:51:22 -0500

    Started Simulation

--- Hightower_Ex10.R
+++ Hightower_Ex10.R
@@ -22,8 +22,9 @@
 
 # Set Parameters
 K=1000000
-timesteps=100
-N0=100 # Given that the mutation occurred when there were 100 cells
+timesteps=200
+N0=99 # Given that the mutation occurred when there were 100 cells
+M0=1 # and only one was a mutant
 
 # Drug absence (A)
 rNA=0.1 # Growth rate for both populations is 0.1 in absence of drug
@@ -37,6 +38,22 @@ rMP=0.05 # Mutants (M) grow at 50% of growth rate for when drug is not present
 # Set initial value
 Ns=numeric(length=timesteps)
 Ns[1]=N0
+Ms=numeric(length=timesteps)
+Ms[1]=M0
 
 # Simulate
+# When drug is not present
+for(t in 1:100){
+  Ns[t+1]=Ns[t]+(rNA*Ns[t])*((1-(Ns[t]+Ms[t]))/K)
+  Ms[t+1]=Ms[t]+(rMA*Ms[t])*((1-(Ns[t]+Ms[t]))/K) 
+}
+# When drug is present
+for(t in 1:100){
+  Ns[t+1]=N[t]+(rN*N[t])*(1-(N[t]+M[t])/K)
+  Ms[t+1]=M[t]+(rM*M[t])*(1-(N[t]+M[t])/K) 
+}
 
+# Put simulation into a dataframe
+library(ggplot2)
+healthy<-data.frame(time=1:timesteps,N=Ns)
+tumor<-data.frame(time=1:timesteps,M=Ms)
